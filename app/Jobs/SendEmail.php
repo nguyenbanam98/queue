b633b95894075365899d27ccd4d5a7d38d4f849f
Nam Nguyen <namnguyenba6298@gmail.com>
2021-03-03 23:42:11 +0700

    v1

---
+++ app/Jobs/SendEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $mail;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $mail
+     */
+    public function __construct($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        Mail::to('email@email.com')->send($this->mail);
+    }
+}
